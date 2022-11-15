a3b58f05b3dc16f0f05a4852bc39c7359d72f74c
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-15 16:30:59 +0600

    hero section added and make it responsive

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <div className=' max-w-7xl mx-auto'>
             <div className=' p-5 hidden lg:flex flex-row items-center justify-between'>
-                <p className=' font-[poppins] font-medium text-xl'>Doctors Portal</p>
+                <p className=' font-[poppins] font-medium text-xl text-black'>Doctors Portal</p>
                 <div className='flex justify-evenly w-[50%] font-[poppins] text-base text-black'>
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
@@ -24,11 +24,11 @@ const Navbar = () => {
                 </div>
             </div>
             <div className=' flex items-center justify-between lg:hidden p-5'>
-                <p className=' font-[poppins] font-medium text-lg'>Doctors Portal</p>
+                <p className=' font-[poppins] font-medium text-lg text-black'>Doctors Portal</p>
                 <div>
                     <img src={toggle ? close : menu} className=" w-6 h-6 object-contain" alt="" onClick={() => setToggle((previous) => !previous)} />
 
-                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-[#181818] absolute top-0 right-0 w-60 h-full flex-col items-end `}>
+                    <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-[#181818] absolute top-0 right-0 w-60 h-full flex-col items-end z-50 `}>
                         <img src={toggle ? close : menu} alt='menu' className='w-[24px] h-[24px] object-contain mb-7 cursor-pointer' onClick={() => setToggle((previous) => !previous)} />
                         <div className='flex flex-col font-[poppins] uppercase md:text-lg text-base text-white z-40 bg-[#181818] w-full mobile-nav-link relative'>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorPortalTheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#37CDBE',
+          neutral: '#3A4256',
+
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
