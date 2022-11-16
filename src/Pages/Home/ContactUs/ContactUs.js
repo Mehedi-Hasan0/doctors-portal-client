1d84d3635559eba655f74ebfdd5da7170df9d33c
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-16 19:35:13 +0600

    react hook form installed and form data validated

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import bgImg from '../../../assets/images/appointment.png';
+import { useForm } from 'react-hook-form';
 
 const ContactUs = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const submit = (data, e) => {
+        console.log(data);
+        const form = e.target;
+        form.reset();
+    }
+
     return (
         <section
             className=' mt-36'
@@ -17,11 +27,11 @@ const ContactUs = () => {
                     <h2 className=" font-[poppins] sm:text-4xl text-2xl text-white font-medium text-center my-6">Stay Connected With Us</h2>
                 </div>
                 <div>
-                    <form>
+                    <form onSubmit={handleSubmit(submit)}>
                         <div className=' flex flex-col lg:w-[50%] sm:w-[70%] w-[90%] mx-auto  text-black'>
-                            <input type="email" name="email" placeholder='Email Address' className=' my-5 p-4 rounded-lg' />
-                            <input type="text" name='subject' placeholder='Subject' className='mb-5 p-4 rounded-lg' />
-                            <input type="text" name='message' placeholder='Your Message' className=' mb-9 py-8 px-4 rounded-lg' />
+                            <input type="email" name="email" placeholder='Email Address' className=' my-5 p-4 rounded-lg' {...register("email", { required: true })} />
+                            <input type="text" name='subject' placeholder='Subject' className='mb-5 p-4 rounded-lg' {...register("submit", { required: true })} />
+                            <input type="text" name='message' placeholder='Your Message' className=' mb-9 py-8 px-4 rounded-lg' {...register("message", { required: true })} />
                         </div>
                         <div className='flex'>
                             <button className="mx-auto btn bg-gradient-to-r from-secondary to-primary text-white border-none">Submit</button>
