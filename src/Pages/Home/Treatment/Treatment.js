5e5e95ba93257a4bbf3c2e93e543ecb22b54466b
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-16 08:28:57 +0600

    treatment section text updated

--- src/Pages/Home/Treatment/Treatment.js
+++ src/Pages/Home/Treatment/Treatment.js
@@ -8,7 +8,7 @@ const Treatment = () => {
                 <img className=' rounded-lg lg:w-[40%] md:w-[80%] w-full lg:mr-24' src={treatmentImg} alt="" />
                 <div className=' md:mx-[9%] lg:mx-0 mt-9 lg:mt-0'>
                     <h2 className='font-[poppins] font-bold lg:text-5xl md:text-4xl text-3xl text-neutral lg:leading-[55px]'>Exceptional Dental Care, on Your Terms</h2>
-                    <p className=' font-[poppins] text-neutral md:text-base text-sm opacity-80 my-8'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <p className=' font-[poppins] text-neutral md:text-base text-sm opacity-80 my-8'>The risk of tooth decay, gum disease and tooth loss can be reduced with good oral hygiene, a low-sugar and acid diet, wearing a mouthguard when playing sport, and regular visits to the dentist. It is generally recommended that everyone, including young children, visit the dentist twice every year. Modern techniques mean that dental treatment is fairly comfortable.</p>
                     <button className="btn bg-gradient-to-r from-secondary to-primary text-white border-none">Get Started</button>
                 </div>
             </div>
