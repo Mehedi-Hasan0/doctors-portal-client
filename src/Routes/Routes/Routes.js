28061c353111f35b79ab944dec834df322d098c6
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-23 10:22:16 +0600

    admin role created

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,10 +3,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import DashboardLayout from '../../Layout/DashboardLayout';
 import Main from '../../Layout/Main';
 import Appointment from '../../Pages/Appointment/Appointment/Appointment';
+import AllUsers from '../../Pages/Dashboard/AllUsers/AllUsers';
 import MyAppointment from '../../Pages/Dashboard/MyAppointment/MyAppointment';
 import Home from '../../Pages/Home/Home/Home';
 import Login from '../../Pages/Login/Login';
 import SignUp from '../../Pages/SignUp/SignUp';
+import AdminRoute from '../AdminRoute/AdminRoute';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 const router = createBrowserRouter([
@@ -39,7 +41,12 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard',
                 element: <MyAppointment />
-            }
+            },
+            {
+                path: '/dashboard/allUsers',
+                element: <AdminRoute><AllUsers /></AdminRoute>
+            },
+
         ]
     }
 ])
